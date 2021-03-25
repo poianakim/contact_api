6ad0eb0723889cb6060e0651ca321ccc707078aa
poiana <poianakim@gmail.com>
2021-03-25 21:55:55 +0100

    implented cors

--- settings.py
+++ settings.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, Response, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 # sqlalchemy 1.4.0 version makes Assertion error so I downgraded it to the (previous) 1.3.x version
 
 app = Flask(__name__)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+@app.route("/")
+@cross_origin()
 ''' 
 If set to True (the default) Flask-SQLAlchemy will track 
 modifications of objects and emit signals. 
